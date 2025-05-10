84fbcfbbddd5e38a9b6b70a8795032607317f419
Marcus Richards <richardsmarcus065@gmail.com>
2025-05-10 14:35:14 +0200

    modifica contenuto sbloccato

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ REQUIRED_CHANNELS = [
     {"tag": "@milanorossonerareplay", "name": "Canale Replay Milan"},
     # Aggiungi altri canali se necessario
 ]
-CONTENT = os.getenv("REWARD_LINK", "Contenuto sbloccato: https://t.me/+5Dub0A9HtXAyNmFk")
+CONTENT = os.getenv("REWARD_LINK", "Contenuto sbloccato: https://t.me/+A4w83yAHKcNlMDc0")
 
 # Crea l'applicazione Telegram con timeout
 application = Application.builder().token(TOKEN).read_timeout(10).write_timeout(10).build()
